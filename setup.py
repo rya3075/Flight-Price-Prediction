e3a79571d99271a8c9ebf48012a139df5b45bfa4
Aarya Nandurkar <ryanandurkar30@gmail.com>
2024-12-05 22:15:37 +0530

    Setup Exception and Logging

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    this function will return the list of requirements
+    """
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+        
+        return requirements
+
+
+
+setup(
+    name='flight_price_prediction_project'
+    version='0.0.1',
+    author= 'Aarya Nandurkar',
+    author_email= 'ryanandurkar30@gmail.com',
+    packages= find_packages(),
+    install_reuires = get_requirements('requirements.txt')
+)
